9a463ae5423e64ab7afec8a69ecdb61a66c12fb9
BillLaptop <earnappbill@gmail.com>
2024-07-17 19:57:40 +0300

    Finish menu page

--- src/menu.js
+++ src/menu.js
@@ -11,24 +11,59 @@ const menu = [
         title: 'BBQ',
         image: 'bbqBurger.jpg',
         desc: 'Beef burger with smoky BBQ sauce and crispy onions.'  
+    },
+    {
+        title: 'Mushroom',
+        image: 'mushroomBurger.jfif',
+        desc: 'Juicy beef burger topped with sautéed mushrooms and Swiss cheese.'
+    },
+    {
+        title: 'Spicy',
+        image: 'spicyBurger.jpg',
+        desc: 'Spicy beef burger with jalapeños and pepper jack cheese.'
+    },
+    {
+        title: 'Veggie',
+        image: 'VeggieBurger.jpg',
+        desc: 'Delicious veggie patty with fresh greens and avocado.'
+    },
+    {
+        title: 'Bacon',
+        image: 'baconBurger.jpg',
+        desc: 'Beef burger loaded with crispy bacon and cheddar cheese.'
     }
 ];
 
-function createMenuPage() {
+
+
+function createMenuPage() {     
+    const mainHeader = document.createElement('h1');
+    mainHeader.textContent = 'Our Burgers';
+    content.appendChild(mainHeader);
+
     const header = document.createElement('h2');
+    const container = document.createElement('div');
+    container.className = 'container';
+    content.appendChild(container);
+
 
     for (let element of menu) {
       const product = document.createElement('div');
+      product.className = 'product';
+
       const image = document.createElement('img');
+      image.className = 'product-image';
       image.src = '/pics/' + element.image;
 
       const productTitle = document.createElement('h3');
+      productTitle.className = 'product-title';
       productTitle.textContent = element.title;
 
       const describe = document.createElement('p');
+      describe.className = 'product-describe';
       describe.textContent = element.desc;
     
-      content.appendChild(product);
+      container.appendChild(product);
       product.appendChild(image);
       product.appendChild(productTitle);
       product.appendChild(describe);
